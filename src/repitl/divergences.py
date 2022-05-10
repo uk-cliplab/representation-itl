0cd1dfc778aa8b78a44143641415574d44467803
KeiderHoyos <keider.hoyos@uky.edu>
2022-05-10 14:28:59 -0400

    Updated divergences with random fourier features

--- src/repitl/divergences.py
+++ src/repitl/divergences.py
@@ -6,11 +6,17 @@ import repitl.kernel_utils as ku
 import repitl.matrix_itl as itl
 import repitl.matrix_itl_approx as approx
 
-def divergenceJR(X,Y,sigma,alpha, weighted = False):
+def divergenceJR(X,Y,sigma,alpha, weighted = False, n_rff = None):
     if weighted:
-        divergence = divergenceJR_weighted(X,Y,sigma,alpha)
+        if n_rff is None:
+            divergence = divergenceJR_weighted(X,Y,sigma,alpha)
+        else:
+            divergence = divergenceJR_weighted_rff(X,Y,sigma,alpha, n_rff)
     else:
-        divergence = divergenceJR_unweighted(X,Y,sigma,alpha)        
+        if n_rff is None:
+            divergence = divergenceJR_unweighted(X,Y,sigma,alpha)  
+        else:
+            divergence = divergenceJR_unweighted_rff(X,Y,sigma,alpha,n_rff)
     return divergence
 
 def divergenceJR_weighted(X,Y,sigma,alpha):
@@ -61,12 +67,41 @@ def divergenceJR_unweighted(X,Y,sigma,alpha):
     Kl = torch.matmul(L, L.t())
     # Computing divergence
     Hxy = itl.matrixAlphaEntropy(K, alpha=alpha)
-    Hj = itl.matrixAlphaJointEntropy([K, Kl_], alpha=alpha)
+    Hj = itl.matrixAlphaJointEntropy([K, Kl], alpha=alpha)
     Hl = approx.matrixAlphaEntropyLabel(L, alpha)
     divergence  = Hxy + Hl - Hj
     return divergence
 
-def divergenceJR_rff(X,Y,sigma,alpha, n_rff):
+def divergenceJR_weighted_rff(X,Y,sigma,alpha, n_rff):
+    # Getting number of samples from each class
+    N = X.shape[0]
+    M = Y.shape[0]
+    # Creating indicator or label variable
+    l = torch.ones(N+M, dtype=torch.long)
+    l[N:] = 0
+    # One-hot encoding the label variable
+    L = torch.nn.functional.one_hot(l).type(X.dtype)
+    # Creating the mixture of the two distributions
+    XY = torch.cat((X,Y))
+    # Approximating covariance matrices
+    D1 = np.sqrt((N+M)/(2*N))*torch.ones(N,1,dtype=X.dtype) # (n1+n2)
+    D2 = np.sqrt((N+M)/(2*M))*torch.ones(M,1,dtype=X.dtype)
+    D = torch.cat((D1,D2))
+    cov_x   = approx.cov_matrix_rff_weighted(X ,sigma,n_rff,D1,random = False)
+    cov_y   = approx.cov_matrix_rff_weighted(Y ,sigma,n_rff,D2,random = False)
+    cov_xy  = approx.cov_matrix_rff_weighted(XY,sigma,n_rff,D ,random = False)
+
+    Hxy = itl.matrixAlphaEntropy(cov_xy, alpha=alpha)
+    # Then, to approximate the joint entropy K॰Kl we use the properties of block diagonal matrices, 
+    # where the trace of the entire matrix is just the sum of the trace of the 2 blocks in the matrix
+    Hj = approx.KzoKl_entropy_rff_weighted(cov_x,cov_y,alpha,sigma,n_rff)
+    # Entropy of the label or indicator variable
+    Hl = approx.matrixAlphaEntropyLabel(L, alpha)
+    # Finally, we compute the divergence
+    divergence = Hxy + Hl - Hj
+    return divergence
+
+def divergenceJR_unweighted_rff(X,Y,sigma,alpha, n_rff):
     # Getting number of samples from each class
     N = X.shape[0]
     M = Y.shape[0]
@@ -84,7 +119,7 @@ def divergenceJR_rff(X,Y,sigma,alpha, n_rff):
     # Then, to approximate the joint entropy K॰Kl we use the properties of block diagonal matrices, 
     # where the trace of the entire matrix is just the sum of the trace of the 2 blocks in the matrix
     # cov_x was computed previously
-    cov_y = approx.cov_matrix_rff(Y,sigma,D,random = False)
+    cov_y = approx.cov_matrix_rff(Y,sigma,n_rff,random = False)
     Hj = approx.KzoKl_entropy_rff(cov_x,cov_y,alpha,sigma,n_rff)
     # Entropy of the label or indicator variable
     Hl = approx.matrixAlphaEntropyLabel(L, alpha)
--- src/repitl/matrix_itl_approx.py
+++ src/repitl/matrix_itl_approx.py
@@ -18,7 +18,7 @@ def Z_rff(X,sigma,n_rff,random = True):
         # Set seed to generate the same set of random weights
         torch.manual_seed(5)
     n_rff = torch.tensor(n_rff)
-    gamma = (1/(2*sigma**2))
+    gamma = torch.tensor(1/(2*sigma**2))
     # Number of features in X
     d = X.shape[1]
     # Random Fourier features
@@ -98,6 +98,23 @@ def KzoKl_entropy_rff(cov_x,cov_y,alpha,sigma,n_rff):
     Hj = (1.0 / (1.0 - alpha)) * torch.log(GIP)
     return Hj
 
+def KzoKl_entropy_rff_weighted(cov_x,cov_y,alpha,sigma):
+    ex, _ = torch.symeig(cov_x, eigenvectors=True)  
+    mx = torch.gt(ex, 0.0)
+    mex = ex[mx]
+    mex /= 2*torch.sum(mex)
+    
+    ey, _ = torch.symeig(cov_y, eigenvectors=True)  
+    my = torch.gt(ey, 0.0)
+    mey = ey[my]
+    mey /= 2*torch.sum(mey)
+    
+    mexy = torch.cat((mex,mey))
+    
+    GIP = torch.sum(torch.exp(alpha * torch.log(mexy))) 
+    Hj = (1.0 / (1.0 - alpha)) * torch.log(GIP)
+    return Hj
+
 def matrixAlphaEntropyLabel(L, alpha, weighted = False):
     '''
     This function compute the entropy of the indicator variable
@@ -156,3 +173,73 @@ def joint_entropy_rff_norm(cov_x,cov_y,alpha,sigma):
     Hj = (1.0 / (1.0 - alpha)) * torch.log(GIP)
     return Hj
 
+
+  
+
+
+
+def generalizedInformationPotential(K, alpha):
+    """Computes the generalized information 
+    potential of order alpha
+          GIP_alpha(K) = trace(K_^alpha), 
+    where K^alpha is a matrix raised to the alpha power. 
+    K_ is normalized as K_ = K / trace(K), such that 
+          trace(K_) = 1.
+   
+    Args:
+      K: (N x N) Gram matrix.
+      alpha: order of the entropy.
+    
+    Returns:
+      GIP: generalized information potential of alpha order. 
+    """
+    ek, _ = torch.symeig(K, eigenvectors=True)  
+    mk = torch.gt(ek, 0.0)
+    mek = ek[mk]
+    mek = mek / torch.sum(mek)
+    GIP = torch.sum(torch.exp(alpha * torch.log(mek))) 
+    return GIP
+
+
+def matrixAlphaEntropy(K, alpha):
+    """Computes the matrix based alpha-entropy
+    based on the spectrum of K
+        H_alpha(A) = (1/(1-alpha))log(trace(A^alpha)), 
+    where A^alpha is the matrix power of alpha (A is normalized).
+    
+    
+    Args:
+      A: (N x N) Gram matrix.
+      alpha: order of the entropy.
+    
+    Returns:
+      H: alpha entropy 
+    """
+    ##ccompute generalized information Potential
+    GIP = generalizedInformationPotential(K, alpha)
+    H = (1.0 / (1.0 - alpha)) * torch.log(GIP)
+    return H
+
+
+
+
+    
+    
+    
+def matrixAlphaJointEntropy(K_list, alpha):
+    """Computes the matrix based alpha joint-entropy
+    based on the spectrum of K
+        H_alpha(K) = (1/(1-alpha))log(trace(K^alpha)), 
+    where K^alpha is the matrix power of K (K is normalized).
+    
+    
+    Args:
+      K_list: a list of (N x N) Gram matrices.
+      alpha: order of the entropy.
+      normalize: Boolean (default True)
+    
+    Returns:
+      H: alpha joint entropy 
+    """
+    K = reduce(lambda x, y: x * y, K_list)
+    return matrixAlphaEntropy(K, alpha)
